8ed149841b8c27d4b921f8f3fa31824414a497db
zerocrazy65 <katapat70@gmail.com>
2023-07-05 09:36:18 +0700

    init

--- 3.longest substring without repeating characters.js
+++ 3.longest substring without repeating characters.js
@@ -1,23 +1,23 @@
 var lengthOfLongestSubstring = function (s) {
-    let firstCount = []
-    let storeCount = []
-    for (var i = 0; i < s.length; i++) {
+   let firstCount = [];
+   let storeCount = [];
+   for (var i = 0; i < s.length; i++) {
       if (firstCount.find((data) => data == s[i])) {
-        const foundIndex = firstCount.findIndex((data) => data == s[i])
-        if (storeCount) {
-          if (storeCount.length < firstCount.length) {
-            storeCount = firstCount
-          }
-          firstCount = firstCount.slice(foundIndex + 1)
-        }
+         const foundIndex = firstCount.findIndex((data) => data == s[i]);
+         if (storeCount) {
+            if (storeCount.length < firstCount.length) {
+               storeCount = firstCount;
+            }
+            firstCount = firstCount.slice(foundIndex + 1);
+         }
       }
-      firstCount.push(s[i])
-    }
-    if (storeCount.length < firstCount.length) {
-      return firstCount.join('').length
-    } else {
-      return storeCount.join('').length
-    }
-  }
+      firstCount.push(s[i]);
+   }
+   if (storeCount.length < firstCount.length) {
+      return firstCount.join("").length;
+   } else {
+      return storeCount.join("").length;
+   }
+};
 
-  console.log(lengthOfLongestSubstring('abcabcbb'))
+console.log(lengthOfLongestSubstring("abcabcbb"));
